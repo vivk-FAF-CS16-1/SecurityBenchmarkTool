b3c8a4b2c131780fbddc77d0bee2f83eaad1b236
Odinlevi <odinlevi@gmail.com>
2021-09-25 19:03:15 +0300

    added buttons + and - in editItemForm, works just fine

--- Form/EditItemForm.Designer.cs
+++ Form/EditItemForm.Designer.cs
@@ -42,6 +42,7 @@ namespace SBT.Form
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.02151F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.97849F));
@@ -54,10 +55,12 @@ namespace SBT.Form
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 96.81818F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(558, 699);
             this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // textBox2
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Location = new System.Drawing.Point(3, 3);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
@@ -66,7 +69,8 @@ namespace SBT.Form
             // 
             // textBox3
             // 
-            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox3.Location = new System.Drawing.Point(204, 3);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
@@ -75,7 +79,8 @@ namespace SBT.Form
             // 
             // fileToolStripMenuItem
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { this.saveToolStripMenuItem });
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
@@ -84,13 +89,14 @@ namespace SBT.Form
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // menuStrip1
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.fileToolStripMenuItem });
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(559, 24);
@@ -114,6 +120,7 @@ namespace SBT.Form
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
--- Form/EditItemForm.cs
+++ Form/EditItemForm.cs
@@ -18,7 +18,10 @@ namespace SBT.Form
         private List<int> _fieldContainer;
 
         private Audit2Struct _audit;
-        
+
+        private int _selectedRowId;
+        private int _uniqueNumber;
+
         public EditItemForm(Audit2Struct audit)
         {
             InitializeComponent();
@@ -27,6 +30,7 @@ namespace SBT.Form
 
             _fieldContainer = new List<int>();
 
+            _selectedRowId = -1; //  audit.Fields.Count != 0 ? audit.Fields.Count - 1 : 0
             UpdateTableLayoutPanel(audit);
         }
 
@@ -38,13 +42,13 @@ namespace SBT.Form
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.Controls.Add(new Label() { Text = "Key" }, 0, 0);
             tableLayoutPanel1.Controls.Add(new Label() { Text = "Value" }, 1, 0);
-
             for (var index = 0; index < audit.Fields.Count; index++)
             {
                 var field = audit.Fields[index];
                 tableLayoutPanel1.RowCount += 1;
                 tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
 
+                
                 var keyTextBox = new RichTextBox();
                 keyTextBox.Text = field.Key;
                 keyTextBox.ReadOnly = false;
@@ -61,13 +65,40 @@ namespace SBT.Form
                 _fieldContainer.Add(index);
 
                 keyTextBox.TextChanged -= FieldKeyModification_handler;
+                keyTextBox.MouseClick -= selectRow_Click;
                 keyTextBox.TextChanged += FieldKeyModification_handler;
+                keyTextBox.MouseClick += selectRow_Click;
 
                 valueTextBox.TextChanged -= FieldValueModification_handler;
+                valueTextBox.MouseClick -= selectRow_Click;
                 valueTextBox.TextChanged += FieldValueModification_handler;
+                valueTextBox.MouseClick += selectRow_Click;
             }
+
+            tableLayoutPanel1.RowCount += 1;
+
+            var buttonsPanel = new TableLayoutPanel();
+
+            var addButton = new Button();
+            addButton.Text = "+";
+            addButton.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+            addButton.MouseClick += addButton_Click;
+
+            var deleteButton = new Button();
+            deleteButton.Text = "-";
+            deleteButton.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+            deleteButton.MouseClick += deleteButton_Click;
+
+            buttonsPanel.RowCount = 1;
+            buttonsPanel.ColumnCount = 2;
+            buttonsPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
+
+            buttonsPanel.Controls.Add(deleteButton, 0, buttonsPanel.RowCount - 1);
+            buttonsPanel.Controls.Add(addButton, 1, buttonsPanel.RowCount - 1);
+
+            tableLayoutPanel1.Controls.Add(buttonsPanel, 1, tableLayoutPanel1.RowCount - 1);
         }
-        
+
         private void FieldKeyModification_handler(object sender, EventArgs e)
         {
             var rowIndex = tableLayoutPanel1.GetRow((Control)sender);
@@ -95,6 +126,45 @@ namespace SBT.Form
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OnUpdate?.Invoke();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("+");
+
+            _audit.Fields.Add(new Audit2Field("New key", "New value"));
+            UpdateTableLayoutPanel(_audit);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-");
+            
+            if (_selectedRowId == -1 || _selectedRowId >= _audit.Fields.Count)
+                return;
+
+            _audit.Fields.RemoveAt(_selectedRowId);
+
+            Console.Write(_audit.Fields);
+            UpdateTableLayoutPanel(_audit);
+        }
+
+        private void selectRow_Click(object sender, EventArgs e)
+        {
+            _selectedRowId = tableLayoutPanel1.GetPositionFromControl((Control)sender).Row - 1;
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                control.BackColor = Color.White;
+            }
+
+            tableLayoutPanel1.GetControlFromPosition(0, _selectedRowId + 1).BackColor = Color.LightBlue;
+            tableLayoutPanel1.GetControlFromPosition(1, _selectedRowId + 1).BackColor = Color.LightBlue;
         }
     }
 }
